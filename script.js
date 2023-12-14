6eb4a91ffcbd012e631ddb8ee6766652563712f5
Pablo Pirata <pablo@studocu.com>
2023-12-14 16:41:50 +0100

    remove unecessary line break

--- script.js
+++ script.js
@@ -407,7 +407,7 @@ function calculateStatistics(dataArray) {
         `Fat Difference: ${fatDifference > 0 ? '+' : ''}${fatDifference?.toFixed(2)} kg`,
         `Non-Fat Difference: ${muscleDifference > 0 ? '+' : ''}${muscleDifference?.toFixed(2)} kg`,
         `Fat Percentage Difference: ${fatDifferencePercentage > 0 ? '+' : ''}${fatDifferencePercentage?.toFixed(2)}%  (${initialFatPercentage?.toFixed(2)} -> ${finalFatPercentage?.toFixed(2)})`,
-        `\nYou're currently ${weightDifference > 0 ? 'gaining' : 'losing'} ${((Math.abs(weightDifference) / totalDays) * 7)?.toFixed(2)}kg per week\n`,
+        `\nYou're currently ${weightDifference > 0 ? 'gaining' : 'losing'} ${((Math.abs(weightDifference) / totalDays) * 7)?.toFixed(2)}kg per week`,
         `\n1% of your body fat is ${(finalFat / finalFatPercentage).toFixed(2)} kg`,
         `Eat ${(tdee - CALORIES_STORED_KG_FAT / 14)?.toFixed(2)} kcal per day to lose 500g of fat per week`,
         `Eat ${(tdee + CALORIES_BUILD_KG_MUSCLE / 14)?.toFixed(2)} kcal per day to gain 500g of muscle per week`,
